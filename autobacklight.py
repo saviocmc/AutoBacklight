22ea71b033c297ba78131749f444946ef2d26210
Sávio Carlos Martins Costa <saviocmc@gmail.com>
2016-03-21 00:43:39 -0300

    Added the constant BASE_LINE to help to ajust the relation between the ambient light level and the backlight level

--- autobacklight.py
+++ autobacklight.py
@@ -5,31 +5,48 @@ import sys
 import cv2
 import time
 
-# Constant that adjust the relation between the ambient light level and the backlight level
-# Should be modified until the user get comfortable with the backlight
-GAIN = 1.2
+# Constants that adjust the relation between the ambient light level and the backlight level
+# Should be modified until the user get comfortable with the backlight and the changes
+#
+# You can think about these constants like the angular coeficient (slope) and the linear coeficient of a straight line equation
+# BackLight = AmbLight*GAIN + BASE_LINE
+GAIN = 1.0
+BASE_LINE = 0.0
+
+# Set the maximum number of steps from the minimum to the maximum value
+# It's useful to avoid little changes constantly
+# TODO
+MAX_STEPS = 10
 
 def main(argv):
 
-	# Test if the backlight sys path is passed, otherwise the Xorg xbacklight will be used
-
-	if (len(argv)==2):
+	# It tests if the backlight sys_path is passed, otherwise the Xorg xbacklight will be used
+	if (len(argv)>=2):
 		SYS_PATH = argv[1]
 		MAX_VALUE = int(open(SYS_PATH+"max_brightness", 'r').read())
 		BRIGHTNESS_FILE = open(SYS_PATH+"brightness", 'w')
 		while True:
 			ambLight = getAmbientLightLevel()
-			print(int(ambLight*GAIN*MAX_VALUE/100))
-			BRIGHTNESS_FILE.write(str(int(ambLight*GAIN*MAX_VALUE/100)))
+			backLight = int(ambLight*GAIN*MAX_VALUE/100 + BASE_LINE)
+			if (backLight < 0): backLight = 0
+			BRIGHTNESS_FILE.write(str(backLight))
 			BRIGHTNESS_FILE.flush()
+
+			#Debug
+			print("AmbLight:\t"+str(ambLight)+"\nBackLight:\t"+str(backLight)+"\n")
+
 			time.sleep(1)
 
 	else:
 		while True:
 			ambLight = getAmbientLightLevel()
-			print(ambLight*GAIN)
+			backLight = int(ambLight*GAIN + BASE_LINE)
 			#Set the screen backlight level whith the command "xbacklight" privided by Xorg.
-			return call(["xbacklight", "-set", str(ambLight*GAIN)])
+			call(["xbacklight", "-set", str(backLight)])
+
+			#Debug
+			print("AmbLight:\t"+str(ambLight)+"\nBackLight:\t"+str(backLight)+"\n")
+
 			time.sleep(1)
 
 def getAmbientLightLevel():
