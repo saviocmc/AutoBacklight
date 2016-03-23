921a14be5c9be72a416f9169010bea5f9df6646b
Sávio Carlos Martins Costa <saviocmc@gmail.com>
2016-03-23 11:07:57 -0300

    Added a function that sets up the camera and gets the image dimension to the getAmbientLightLevel function

--- autobacklight.py
+++ autobacklight.py
@@ -5,21 +5,21 @@ import cv2
 import time
 
 # Indicates the directory in your system where are the files that actually controls the backlight.
-# Usualy, it is "/sys/class/backlight/something"
+# Usually, it is "/sys/class/backlight/something"
 # The directory must have the files "brightness" and "max_brightness" inside it.
-SYS_PATH = "/sys/class/backlight/acpi_video0"
+SYS_PATH = ""
 
 # Constants that adjust the relation between the ambient light level and the backlight level
 # Should be modified until the user get comfortable with the backlight and the changes
 #
-# You can think about these constants like the angular coeficient (slope) and the linear coeficient of a straight line equation
+# You can think about these constants like the angular coefficient (slope) and the linear coeficient of a straight line equation
 # BackLight = AmbLight*GAIN + BASE_LINE
 GAIN = 1.0
 BASE_LINE = 0.0
 
 # The number of images captured per minute
-# It is also the number of ajustmens per minute that the app is gonna do
-FREQUENCY = 120
+# It is also the number of adjustments per minute that this app is gonna do
+FREQUENCY = 60
 
 # Set the maximum number of steps from the minimum to the maximum value of brightness
 # It's useful to avoid constantly little changes
@@ -27,9 +27,10 @@ FREQUENCY = 120
 MAX_STEPS = 10
 
 def main(argv):
-	# First, it tests if the SYS_PATH is passed
 
-	if (len(SYS_PATH) > 0): # Using the system file 'brightness' under SYS_PATH to control the backlight
+	setUpCamera()
+
+	if (len(SYS_PATH) > 0): # It's gonna use the system file 'brightness' under SYS_PATH to control the backlight
 		global SYS_PATH
 		if(not SYS_PATH.endswith("/")): SYS_PATH += "/"
 		MAX_VALUE = int(open(SYS_PATH + "max_brightness", 'r').read())
@@ -45,7 +46,7 @@ def main(argv):
 			print("AmbLight:\t"+str(ambLight)+"\nBackLight:\t"+str(backLight)+"\n")
 			time.sleep(60/FREQUENCY)
 
-	else: # Using the command "xbacklight" privided by Xorg
+	else: # It's gonna use the command "xbacklight" privided by Xorg
 		while True:
 			ambLight = getAmbientLightLevel()
 			backLight = int(ambLight*GAIN + BASE_LINE)
@@ -56,33 +57,35 @@ def main(argv):
 
 def getAmbientLightLevel():
 
-	# Get the camera at location 0
-	cam = cv2.VideoCapture(0)
-
-	returnStatus, image = cam.read()
-
-	# Get the height and the width (in pixels) of the image
-	height = len(image)
-	width = len(image[0])
+	returnStatus, image = getAmbientLightLevel.cam.read()
 
 	# Variable that will store the light intensity, calculated from the image pixels
 	lightIntensity = 0
 
 	# Get the central row of the image, which will be analyzed
 	# Experimentally, I concluded that one row is sufficient to estimate the light intensity. Analysing the whole image is a waste of CPU power.
-	centralRow = image[height/2]
+	centralRow = image[getAmbientLightLevel.img_height/2]
 	for pixel in centralRow:
 		# In the HSV color space, the Intensity of a color (or a pixel), is giving by the max RGB value.
 		# https://en.wikipedia.org/wiki/HSL_and_HSV
 		lightIntensity += max(pixel)
 
 	# Normalize the value to a scale of one pixel (0-255)
-	lightIntensity /= width
+	lightIntensity /= getAmbientLightLevel.img_width
 
 	# Normalize the value to a scale of 0-100
 	lightIntensity /= 2.55
 
 	return  lightIntensity
+
+def setUpCamera():
+
+	# Get the default camera (for laptops, it's Usually the built-in camera)
+	getAmbientLightLevel.cam = cv2.VideoCapture(0)
+
+	# Get the height and the width (in pixels) of the camera images
+	getAmbientLightLevel.img_width = getAmbientLightLevel.cam.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+	getAmbientLightLevel.img_height = getAmbientLightLevel.cam.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
 
 if __name__ == '__main__':
 	main(sys.argv)
